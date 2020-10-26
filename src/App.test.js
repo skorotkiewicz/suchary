422ee953c6e78c27046fdb677b2b88a72899c467
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2020-10-26 16:34:15 +0100

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,11 @@ import App from "./App";
 
 import allReducer from "./_reducers";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import reducers from "./_reducers";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 const Root = () => {
-  const store = createStore(allReducer);
+  const store = createStore(allReducer, applyMiddleware(thunk));
   return (
     <Provider store={store}>
       <App />
@@ -20,39 +20,4 @@ test("render Index page", () => {
   const { getByText } = render(<Root />);
   const linkElement = getByText(/najnowsze/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-test("reducers", () => {
-  let state;
-  state = reducers(
-    {
-      favorites: ["5f7b99d31a88460007aacc81", "5f87ce327dd17200087a5261"],
-      likes: [
-        "5f882944becb390007275650",
-        "5f88299b07fa090008d73db7",
-        "5f87ce327dd17200087a5261",
-      ],
-      auth: {
-        auth: "123",
-        login: "tester",
-      },
-      page: 1,
-      isLoading: false,
-    },
-    { type: "setIsLoading", payload: true }
-  );
-  expect(state).toEqual({
-    favorites: ["5f7b99d31a88460007aacc81", "5f87ce327dd17200087a5261"],
-    likes: [
-      "5f882944becb390007275650",
-      "5f88299b07fa090008d73db7",
-      "5f87ce327dd17200087a5261",
-    ],
-    auth: {
-      auth: "123",
-      login: "tester",
-    },
-    page: 1,
-    isLoading: true,
-  });
 });
